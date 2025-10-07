cc0d0186a5ce13728f442a7ed4885e0fcd723323
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-10-07 12:07:31 -0600

    Update bulletify2.py

--- bulletify2.py
+++ bulletify2.py
@@ -1,1 +1,20 @@
+import pyperclip
+
+# Get text from clipboard
+text = pyperclip.paste()
+
+# Separate lines
+lines = text.split('\n')
+
+# Loop through all indexes in the "lines" list
+for i in range(len(lines)):
+    # Add a star only if the line is not empty
+    if lines[i].strip() != '':
+        lines[i] = '* ' + lines[i]
+
+# Join the lines back into a single string
+text = '\n'.join(lines)
+
+# Copy the result back to the clipboard
+pyperclip.copy(text)
 
